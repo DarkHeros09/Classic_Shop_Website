5203220f252ee5b0ca6b1681dbf4bc7b475ed8f6
Mohammed Najib <mnbenghuzzi@gmail.com>
2026-01-11 23:32:57 +0200

    test deployment round 6

--- view/layout/header_templ.go
+++ view/layout/header_templ.go
@@ -31,7 +31,7 @@ func Header(lang string, content model.Content) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<meta charset=\"UTF-8\"><base href=\"/\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Classic Shop</title><meta name=\"description\" content=\"")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<meta charset=\"UTF-8\"><base href=\"/Classic_Shop_Website/\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Classic Shop</title><meta name=\"description\" content=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
